43026e74af66733a9430c63cf430f2a001fcecaf
Andrey Adamovich <andrey@aestasit.com>
2018-11-01 16:42:26 +0200

    refactored

--- src/com/playtech/Main.java
+++ src/com/playtech/Main.java
@@ -1,55 +1,62 @@
 package com.playtech;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
+
+import static java.nio.file.Files.lines;
+import static java.nio.file.Paths.get;
+import static java.util.stream.Collectors.toList;
 
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        List<Train> trains = new ArrayList<>();
-        List<String> lines = new ArrayList<>();
-        lines = Files.lines(Paths.get("schedule.txt"))
-                .collect(Collectors.toList());
+  public static class TrainManager {
 
-        for (String str : lines) {
-            String[] chunks = str.split(" ");
-            Map<String, LocalTime> schedule = new TreeMap<>();
-            Map<String, Integer> stopNrs = new TreeMap<>();
-            for (int i = 1; i < chunks.length; i++) {
-                String[] entry = chunks[i].split("=");
-                schedule.put(entry[0], LocalTime.parse(entry[1]));
-                stopNrs.put(entry[0], i);
-            }
-            trains.add(new Train(chunks[0], schedule, stopNrs));
-        }
-        trains.forEach(System.out::println);
-        isDirectConnection(trains, "CLU", "ADC");
-        isShortTrip(trains, "CLU", "DED");
-    }
+    List<Train> trains = new ArrayList<>();
 
-    public static void isDirectConnection(List<Train> trains, String station1, String station2) {
-        trains
-                .stream()
-                .filter(s -> s.getSchedule().containsKey(station1) && s.getSchedule().containsKey(station2))
-                .filter(s -> s.getSchedule().get(station1).isBefore(s.getSchedule().get(station2)))
-                .forEach(s -> System.out.println(s.getId()));
-    }
+  }
 
-    public static void isShortTrip(List<Train> trains, String station1, String station2) {
-        trains
-                .stream()
-                .filter(s -> s.getSchedule().containsKey(station1) && s.getSchedule().containsKey(station2))
-                .filter(s -> s.getSchedule().get(station1).isBefore(s.getSchedule().get(station2)))
-                .filter(s -> (s.getStopNrs().get(station2) - s.getStopNrs().get(station1)) <= 2)
-                .forEach(s -> System.out.println(s.getId()));
+  public static void main(String[] args) throws IOException {
 
-    }
+    List<Train> trains = lines(get("schedule.txt"))
+        .map((String line) -> line.split(" "))
+        .map((String[] chunks) -> addTrainSchedule(new Train(chunks[0]), chunks))
+        .collect(toList());
+
+    trains.forEach(System.out::println);
+
+    isDirectConnection(trains, "CLU", "ADC");
+    isShortTrip(trains, "CLU", "DED");
+
+  }
+
+  private static Train addTrainSchedule(Train newTrain, String[] chunks) {
+    Arrays
+        .stream(chunks)
+        .skip(1)
+        .map(chunk -> chunk.split("=", 2))
+        .forEach((String[] entry) ->
+            newTrain.addStation(entry[0], LocalTime.parse(entry[1])));
+    return newTrain;
+  }
+
+  public static void isDirectConnection(List<Train> trains, String station1, String station2) {
+    trains
+        .stream()
+        .filter(s -> s.getSchedule().containsKey(station1) && s.getSchedule().containsKey(station2))
+        .filter(s -> s.getSchedule().get(station1).isBefore(s.getSchedule().get(station2)))
+        .forEach(s -> System.out.println(s.getId()));
+  }
+
+  public static void isShortTrip(List<Train> trains, String station1, String station2) {
+    trains
+        .stream()
+        .filter(train -> train.isShortTrip(station1, station2))
+        .forEach(System.out::println);
+
+  }
+
 }
--- src/com/playtech/Train.java
+++ src/com/playtech/Train.java
@@ -1,40 +1,52 @@
 package com.playtech;
 
+import javax.util.streamex.StreamEx;
 import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.joining;
 
 public class Train {
-    private String id;
-    //private List<ScheduleEntry> schedule;
-    private Map<String, LocalTime> schedule;
-    private Map<String, Integer> stopNrs;
 
-    public Train(String id, Map<String, LocalTime> schedule, Map<String, Integer> stopNrs) {
-        this.id = id;
-        this.schedule = schedule;
-        this.stopNrs = stopNrs;
+  private String id;
+  private Map<String, LocalTime> schedule = new LinkedHashMap<>();
 
-    }
+  public Train(String id) {
+    this.id = id;
+  }
 
-    public Map<String, Integer> getStopNrs() {
-        return stopNrs;
-    }
+  public Train addStation(String stationName, LocalTime time) {
+    schedule.put(stationName, time);
+    return this;
+  }
 
-    public String getId() {
-        return id;
-    }
+  public String getId() {
+    return id;
+  }
 
-    public Map<String, LocalTime> getSchedule() {
-        return schedule;
-    }
+  public Map<String, LocalTime> getSchedule() {
+    return schedule;
+  }
 
-    @Override
-    public String toString() {
-        return "Train{" +
-                "id='" + id + '\'' +
-                ", schedule=" + schedule +
-                '}';
-    }
+  public boolean isShortTrip(String station1, String station2) {
+    return StreamEx.of(schedule.keySet())
+        .takeWhile(station -> station.equals(station1))
+        .dropWhile(station -> station.equals(station2))
+        .count() <= 2;
+  }
+
+  @Override
+  public String toString() {
+    return "Train{" +
+        "id='" + id + '\'' +
+        ", schedule=" + schedule
+        .entrySet()
+        .stream()
+        .map(entry -> entry.getKey() + "=" + entry.getValue()).collect(joining()) +
+        '}';
+  }
 }
